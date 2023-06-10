b8c98614a131e1f3f4b7d52f72576aba5a8d033e
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2023-06-10 16:28:54 +0530

    Custom User

--- travelplanner/planner/models.py
+++ travelplanner/planner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    Name=models.CharField(max_length=200,null=True,unique=True)
+    Location=models.CharField(max_length=200,null=True)
+    Preference=models.CharField(max_length=200,null=True)
+
+    USERNAME_FIELD='Name'
+    REQUIRED_FIELDS=[]
+
+    def __str__(self):
+        return self.username
+    
