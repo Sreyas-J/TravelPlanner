1afd8d5e4504a148e45d1ccba5671646d25c39b8
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2023-06-11 12:47:02 +0530

    login

--- travelplanner/planner/urls.py
+++ travelplanner/planner/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('home/', views.index, name="index"),
-    path('plan/', views.plan, name="plan"),
-    path('about/', views.about, name="about"),
+    path('', views.index, name="index"),
+    path('login/', views.loginView,name="login"),
+    path('plan/', views.plan,name="plan"),
+    path('about/', views.plan,name="about"),
     path('contact/', views.contact, name="contact"),
 ]
--- travelplanner/planner/views.py
+++ travelplanner/planner/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -15,3 +17,24 @@ def about(request):
 
 def contact(request):
     return render(request,"contact.html")
+
+def loginView(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request, "User doesn't exist")
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect("index")
+
+        else:
+            messages.error(request, "Incorrect Password")
+
+    return render(request,"login.html")
