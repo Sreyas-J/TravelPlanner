6f6c7376cdd7b6e4b4583e49fba14e9cde6c4368
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2023-06-14 11:48:24 +0530

    Flight Model

--- travelplanner/planner/models.py
+++ travelplanner/planner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flight_name=models.CharField(max_length=50,null=True)
+    from_loc=models.CharField(max_length=100,null=True)
+    to=models.CharField(max_length=100,null=True)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f'{self.flight_name}:from {self.from_loc} to {self.to} at {self.departure_time} - {self.arrival_time} for Rs.{self.price}'
